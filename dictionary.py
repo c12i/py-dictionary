b6ded7a2b85c35ed64edd9a0de7ca31e0ff30463
Collins Muriuki <murerwacollins@gmail.com>
2019-10-04 16:09:14 +0300

    Adds milti-line printing

--- dictionary.py
+++ dictionary.py
@@ -6,15 +6,44 @@ from difflib import get_close_matches
 handle = open("data.json")
 data = json.load(handle)
 
+print(
+'''
+Term-Dict (>__<)
+
+'''
+    )
+
+print(
+'''
+A handy dictinary tool accessed through the terminal:
+
+=====================
+|                   |
+|                   |
+|    DICTIONARY     |
+|                   |
+|                   |
+=====================
+'''
+    )
+
 def search(word=input("Enter a word to search: ").lower()):
     # an array of close matches, with a cutoff of 0.6
     close = get_close_matches(word, data.keys(), cutoff=0.7)
 
     if word in data:
         print(data[word])
+        print("-----------------------------------------")
     elif len(close) > 0:
         # returns the value at index 0, the closest match
-        print("Oops, did you mean {} instead?".format(close[0]))
+        print("------------------")
+        response = input("Oops, did you mean {} instead? Enter 'Y' if yes and 'N' if no: ".format(close[0])).lower()
+        if response.startswith("y"):
+            word = close[0]
+            print(data[word])
+            print("-----------------------------------------")
+        else:
+            pass       
     else:
         print("The word does not exist, please check again.")
 
